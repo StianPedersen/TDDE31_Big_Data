fb62c7a2c8e3ea0e751ddaaaa7db2d53a67c3cc6
SLP <stian@stian.com>
2023-04-28 12:05:35 +0200

    ashfahf

--- lab2-sparksql/lab2_1.py
+++ lab2-sparksql/lab2_1.py
@@ -6,7 +6,7 @@ sc = SparkContext(appName = "Lab2")
 sqlContext = SQLContext(sc)
 
 #Loading text file and convert each line to a Row
-temperature_file = sc.textFile("BDA/input/temperature-readings-small.csv")
+temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
 lines = temperature_file.map(lambda line: line.split(";"))
 
 tempReadings = lines.map(lambda p: Row(station=p[0], date=p[1], year=int(p[1].split("-")[0]), time=p[2], temp=float(p[3]), quality=p[4]))
