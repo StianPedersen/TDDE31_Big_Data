7614d5de24b62882aa0b2e63db4ffdfef1e9fec7
SLP <stian@stian.com>
2023-05-04 09:12:02 +0200

    Lab 2.5 done

--- lab2-sparksql/lab2_5.py
+++ lab2-sparksql/lab2_5.py
@@ -11,18 +11,20 @@ ostergotland_file = sc.textFile("BDA/input/stations-Ostergotland.csv")
 lines_p = precipation_file.map(lambda line: line.split(";"))
 lines_o = ostergotland_file.map(lambda line: line.split(";"))
 
-perc = lines_p.map(lambda p: Row(station=p[0], date=p[1], perc=float(p[3])))
+perc = lines_p.map(lambda p: Row(station=p[0], year=int(p[1].split("-")[0]),month = int(p[1].split("-")[1]), perc=float(p[3])))
 ost_stations = lines_o.map(lambda p: Row(station=p[0]))
 perc = sqlContext.createDataFrame(perc)
-ost_stations = ost_stations.createDataFrame(perc)
+ost_stations = sqlContext.createDataFrame(ost_stations)
 
 perc.registerTempTable("precipitation")
 ost_stations.registerTempTable("ost_stations")
 
 perc_filtered = perc.filter( (perc['year'] >= 1993) & (perc['year'] <= 2016))
 perc_grouped = perc_filtered.groupBy(perc_filtered['year'],perc_filtered['month'],perc_filtered['station']).agg(F.sum(perc_filtered['perc']).alias('sum_rain'))
-perc_grouped_ym = perc_grouped.join(perc, ['year', 'month'],'inner')
-
+perc_grouped_ym = perc_grouped.join(ost_stations, ['station'],'inner').select('year','month','sum_rain')
+# Should have all the correct stations
+perc_grouped_ym = perc_grouped_ym.groupBy(perc_grouped_ym['year'],perc_grouped_ym['month']).agg(F.avg(perc_grouped_ym['sum_rain']).alias('avg_rain'))
+lab2_5 = perc_grouped_ym.orderBy(perc_grouped_ym['year'],perc_grouped_ym['month'],ascending=False)
 # last
-last = perc_grouped_ym.rdd
+last = lab2_5.rdd
 last.saveAsTextFile("BDA/output")
