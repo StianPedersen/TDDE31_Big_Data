3565a97bc962c19ebb93799074ba62bf9e4e091b
SLP <stian@stian.com>
2023-05-09 09:01:36 +0200

    redone lab 2_3

--- lab2-sparksql/lab2_3.py
+++ lab2-sparksql/lab2_3.py
@@ -9,16 +9,21 @@ sqlContext = SQLContext(sc)
 temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
 lines = temperature_file.map(lambda line: line.split(";"))
 
-tempReadings = lines.map(lambda p: Row(station=p[0], date=p[1], year=int(p[1].split("-")[0]),month = int(p[1].split("-")[1]), time=p[2], temp=float(p[3]), quality=p[4]))
+tempReadings = lines.map(lambda p: Row(station=p[0],day=int(p[1].split("-")[2]), date=p[1], year=int(p[1].split("-")[0]),month = int(p[1].split("-")[1]), time=p[2], temp=float(p[3]), quality=p[4]))
 
 schemaTempReadings = sqlContext.createDataFrame(tempReadings)
 
 schemaTempReadings.registerTempTable("tempReadings")
 
 tmp_schema = schemaTempReadings.filter( (schemaTempReadings['year'] >= 1960) & (schemaTempReadings['year'] <= 2014))
+min = tmp_schema.groupBy(tmp_schema['year'],tmp_schema['month'],tmp_schema['day'],tmp_schema['station']).agg(F.min(tmp_schema['temp']).alias('mintemp'))
+max = tmp_schema.groupBy(tmp_schema['year'],tmp_schema['month'],tmp_schema['day'],tmp_schema['station']).agg(F.max(tmp_schema['temp']).alias('maxtemp'))
+joined = min.join(max, ['year', 'month','day', 'station'], 'inner')
+joined = joined.select(joined['year'],joined['month'],joined['day'],joined['station'], ((joined['mintemp'] + joined['maxtemp'])/2).alias('avgtemp'))
 
-lab2_3 = tmp_schema.groupBy(tmp_schema['year'],tmp_schema['month'],tmp_schema['station']).agg(F.avg(tmp_schema['temp']).alias('avgtemp'))
+lab2_3 = joined.groupBy(tmp_schema['year'],tmp_schema['month'],tmp_schema['station']).agg(F.avg(joined['avgtemp']).alias('avgtemp'))
 lab2_3 = lab2_3.orderBy(lab2_3['avgtemp'],ascending=False)
+
 print("ABCDE")
 lab2_3.show()
 last = lab2_3.rdd
